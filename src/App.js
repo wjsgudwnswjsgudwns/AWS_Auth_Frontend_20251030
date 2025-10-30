66dec77c5c2e069ec5becb9c983048a7fdeb57b1
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-30 11:46:06 +0900

    사용자 정보 출력

--- src/App.js
+++ src/App.js
@@ -46,6 +46,16 @@ function App() {
     }
   };
 
+  //사용자 확인
+  const userCheck = async () => {
+    try {
+      const res = await api.get("/api/auth/me");
+      setMessage("현재 로그인 중인 아이디 : " + res.data.username);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
       <h1>회원 로그인</h1>
@@ -60,11 +70,18 @@ function App() {
         onChange={(e) => setPassword(e.target.value)}
       ></input>
       <br></br>
+
       <button onClick={signup}>회원가입</button>
       <hr></hr>
+
       <button onClick={login}>로그인</button>
       <button onClick={logout}>로그아웃</button>
       <hr></hr>
+
+      <button onClick={userCheck}>사용자 확인</button>
+
+      <hr></hr>
+
       <h2>백 앤드 응답 : {message}</h2>
     </div>
   );
